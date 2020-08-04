fb56c7ad2903bba590d3951f009daa293aa6f46f
Alexandr Litvinenko <snowwasabi@icloud.com>
2020-08-05 02:44:29 +0600

    ignore

--- anime.js
+++ anime.js
@@ -1,16 +1,22 @@
+/*Тест кнопки*/
+	function exit() {
+		alert("hi");
+	}
+
 window.onload = function () {
 
-var t = setInterval(move,10);
-var pos = 0;
-var box = document.getElementById("box");
+/*Тестовая анимация*/
+	var t = setInterval(move,10);
+	var pos = 0;
+	var box = document.getElementById("box");
 
-function move() {
-	if (pos >= 150) {
-		clearInterval(t);
+	function move() {
+		if (pos >= 900) {
+			clearInterval(t);
+		}
+		else {
+			pos +=1;
+			box.style.left = pos+"px";
+		}
 	}
-	else {
-		pos +=1;
-		box.style.left = pos+"px";
-	}
-}
 }
